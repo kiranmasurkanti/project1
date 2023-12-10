11921c528409826dd8c132a73121f59be1f81dcf
kiranmasurakanti <masurakantikiranyadav@gmail.com>
2023-12-10 23:58:40 +0530

    hi

---
+++ src/com/edubridge/myproject1/util/Calc.java
@@ -0,0 +1,12 @@
+package com.edubridge.myproject1.util;
+
+public class Calc {
+	public static int findsquare(int num){
+		return num*num;
+		
+	}
+	public static int findCube(int num) {
+		return num*num*num;
+		
+	}
+}
---
+++ src/com/edubridge/myproject1/util/IntrestCalc.java
@@ -0,0 +1,9 @@
+package com.edubridge.myproject1.util;
+
+public class IntrestCalc {
+	public static double findSimpleInterest(double p, double t, double r) {
+		return (p*t*r)/100;
+		
+	}
+
+}
